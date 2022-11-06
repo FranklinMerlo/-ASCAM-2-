2a9a2361578f7390380c031132c968810b548a91
BiscuitPage <73861310+FranklinMerlo@users.noreply.github.com>
2022-11-06 08:30:34 +0000

    madeitmuchmuchworse

--- send.py
+++ send.py
@@ -6,17 +6,21 @@ import time
 coolcount = 0
 
 def sendmail():
-  user = 'realbusinessproposal@gmail.com'
-  app_password = 'tpdgpzbbkvjybhby' # a token for gmail
-  to = ['merlof22storage@gmail.com', 'mables1026@gmail.com', 'michaelhorvath896@gmail.com', 'mattdcir@gmail.com', 'fjmerlo@buffalo.edu']
+  for i in range(10):
+  
+    user = 'realbusinessproposal@gmail.com'
+    app_password = 'tpdgpzbbkvjybhby' # a token for gmail
+    to = ['mables1026@gmail.com', 'michaelhorvath896@gmail.com', 'mattdcir@gmail.com', 'fjmerlo@buffalo.edu']
+  
+    subject = 'ATTENTION!!!' + '------------------------------------' + str(time.time())
+    f = open('completed.txt', 'r')
+    response = f.read()
+    f.close()
+    content = [response]
+  
+    with yagmail.SMTP(user, app_password) as yag:
+      for name in to:
+        yag.send(name, subject, content)
+        print('Sent email successfully')
 
-  subject = 'ATTENTION!!!' + '------------------------------------' + str(time.time())
-  f = open('completed.txt', 'r')
-  response = f.read()
-  f.close()
-  content = [response]
 
-  with yagmail.SMTP(user, app_password) as yag:
-    for name in to:
-      yag.send(name, subject, content)
-      print('Sent email successfully')

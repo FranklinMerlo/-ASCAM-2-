cb733e248510dc341e383b7d34c8ca5d843d1a6c
BiscuitPage <73861310+FranklinMerlo@users.noreply.github.com>
2022-11-06 12:33:20 +0000

    new1

--- send.py
+++ send.py
@@ -4,11 +4,11 @@ import yagmail
 import time
 
 def sendmail():
-  for i in range(1):
+  for i in range(30):
   
     user = 'notarealbusinessorami@gmail.com'
     app_password = 'cljuacexlfvgcxza' # a token for gmail
-    to = ['mables1026@gmail.com', 'michaelhorvath896@gmail.com', 'mattdcir@gmail.com', 'fjmerlo@buffalo.edu', 'hrhutton@buffalo.edu', 'liammull@buffalo.edu', 'mwhorvat@buffalo.edu', 'irenemae@buffalo.edu', 'mdciraol@buffalo.edu'] #Any targets go here
+    to = ['mables1026@gmail.com', 'michaelhorvath896@gmail.com'] #Any targets go here
   
     subject = 'ATTENTION!!!' + '------------------------------------' + str(time.time()) #This ensures every email is fresh and new, not a chain
     f = open('completed.txt', 'r')

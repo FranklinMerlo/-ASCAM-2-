c680c52c27a378f70ca206bc65a97dda9819a895
BiscuitPage <73861310+FranklinMerlo@users.noreply.github.com>
2022-11-09 15:32:34 +0000

    changedemailsendtoworkagain

--- send.py
+++ send.py
@@ -1,14 +1,14 @@
-#bamrkgiodcgrvocd
+#
 
 import yagmail
 import time
 
 def sendmail():
-  for i in range(3):
+  for i in range(1):
   
-    user = 'username@gmail.com'
-    app_password = '' # a token for gmail
-    to = [''] #Any targets go here
+    user = 'realbusinessproposal@gmail.com'
+    app_password = 'viqfkuziayaynmdc' # a token for gmail
+    to = ['notharrisonhutton2@gmail.com'] #Any targets go here
   
     subject = 'ATTENTION!!!' + '------------------------------------' + str(time.time()) #This ensures every email is fresh and new, not a chain
     f = open('completed.txt', 'r')
